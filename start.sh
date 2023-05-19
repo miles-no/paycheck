6e4fafb19730f5bdb01091b4f9c579df1ea0f1cd
Povilas Galčius <povilas.galcius@komponent.no>
2023-05-19 13:19:02 +0300

    Fix start script comments

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# This file is how Fly starts the server (configured in fly.toml). Before starting
-# the server though, we need to run any prisma migrations that haven't yet been
-# run, which is why this file exists in the first place.
-# Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
+echo This file is how Fly starts the server (configured in fly.toml). Before starting
+echo the server though, we need to run any prisma migrations that haven't yet been
+echo run, which is why this file exists in the first place.
+echo Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
 npx prisma migrate deploy
