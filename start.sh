452c20c5edfe79614319e24a878d28311817d773
Povilas Galčius <povilas.galcius@komponent.no>
2023-05-19 16:39:08 +0300

    Removed start script comments

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
-
-echo This file is how Fly starts the server (configured in fly.toml). Before starting
-echo the server though, we need to run any prisma migrations that haven't yet been
-echo run, which is why this file exists in the first place.
-echo Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
-
 set -ex
 npx prisma migrate deploy
 npm run start
